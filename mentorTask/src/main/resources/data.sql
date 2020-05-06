1093f672b9c99aaff76304210560aaade7ffc9a1
Pabitrakumar Mondal <pabitrakumar.mondal@cognizant.com>
2020-05-06 22:58:00 +0530

    create table for country, state, city

--- mentorTask/src/main/resources/data.sql
+++ mentorTask/src/main/resources/data.sql
@@ -1,2 +1,42 @@
-insert into user values (101,'SSBala235', 'MrX@1125');
-insert into user values (102,'AKDon256', 'iamX@185');
+-- Include data in user table
+insert into user(user_name, password) values('SSBala235', 'MrX_1125');
+insert into user(user_name, password) values('AKDon256', 'iamX_185');
+
+-- Include data in country table
+insert into country(co_name) values('India');
+insert into country(co_name) values('USA');
+
+-- Include data in state table
+insert into state(st_name, st_co_id) values('Karnataka','1');
+insert into state(st_name, st_co_id) values('Tamil Nadu','1');
+insert into state(st_name, st_co_id) values('West Bengal','1');
+
+insert into state(st_name, st_co_id) values('California','2');
+insert into state(st_name, st_co_id) values('New York','2');
+insert into state(st_name, st_co_id) values('Texas','2');
+
+-- Include data in city table
+insert into city(ct_name, ct_st_id) values('Bangalore','1');
+insert into city(ct_name, ct_st_id) values('Mangalore','1');
+insert into city(ct_name, ct_st_id) values('Mysore','1');
+
+insert into city(ct_name, ct_st_id) values('Chennai','2');
+insert into city(ct_name, ct_st_id) values('Coimbatore','2');
+
+insert into city(ct_name, ct_st_id) values('Asansol','3');
+insert into city(ct_name, ct_st_id) values('Durgapur','3');
+insert into city(ct_name, ct_st_id) values('Haldia','3');
+insert into city(ct_name, ct_st_id) values('Kharagpur','3');
+insert into city(ct_name, ct_st_id) values('Kolkata','3');
+
+insert into city(ct_name, ct_st_id) values('Santa Cruz','4');
+insert into city(ct_name, ct_st_id) values('Compton','4');
+insert into city(ct_name, ct_st_id) values('Malibu','4');
+
+insert into city(ct_name, ct_st_id) values('Albany','5');
+insert into city(ct_name, ct_st_id) values('Buffalo','5');
+insert into city(ct_name, ct_st_id) values('New York','5');
+
+insert into city(ct_name, ct_st_id) values('Dallas','6');
+insert into city(ct_name, ct_st_id) values('Edinburg','6');
+insert into city(ct_name, ct_st_id) values('Texas City','6');
--- mentorTask/src/main/resources/schema.sql
+++ mentorTask/src/main/resources/schema.sql
@@ -1,6 +1,39 @@
-drop table if exists user;
-create table user(
-	user_id int(3) primary key,
-	user_name varchar(20) not null,
-	password varchar(8) not null
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`(
+	`user_id` int PRIMARY KEY AUTO_INCREMENT,
+	`user_name` varchar(20) NOT NULL,
+	`password` varchar(8) NOT NULL
+);
+
+DROP TABLE IF EXISTS `country`;
+CREATE TABLE `country`(
+	`co_id` int PRIMARY KEY AUTO_INCREMENT,
+	`co_name` VARCHAR(20) NOT NULL
+);
+
+DROP TABLE IF EXISTS `state`;
+CREATE TABLE `state`(
+	`st_id` int PRIMARY KEY AUTO_INCREMENT,
+	`st_name` VARCHAR(20) NOT NULL,
+	`st_co_id` int NOT NULL,
+	FOREIGN KEY (`st_co_id`) REFERENCES country(`country_id`)
+);
+DROP TABLE IF EXISTS `city`;
+CREATE TABLE `city`(
+	`ct_id` int PRIMARY KEY AUTO_INCREMENT,
+	`ct_name` VARCHAR(20) NOT NULL,
+	`ct_st_id` int NOT NULL,
+	FOREIGN KEY (`ct_st_id`) REFERENCES `state`(`st_id`)
+);
+
+DROP TABLE IF EXISTS `intern`;
+CREATE TABLE `intern`(
+	`in_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`in_name` VARCHAR(20) NOT NULL,
+	`in_company_name` VARCHAR(20) NOT NULL,
+	`in_contact_number` VARCHAR(20) NOT NULL,
+	`in_gender` VARCHAR(6) NOT NULL,
+	`in_country` VARCHAR(20) NOT NULL,
+	`in_state` VARCHAR(20) NOT NULL,
+	`in_city` VARCHAR(20) NOT NULL
 );
